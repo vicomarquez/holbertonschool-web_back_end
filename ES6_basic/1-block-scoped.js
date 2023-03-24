f7f859055bcfc23cd4982b1db76085ea32932b0a
vicomarquez <vicomarquezmuniz@gmail.com>
2023-03-24 20:01:08 -0300

    fix task 1

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -2,11 +2,12 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
+  /* eslint-disable no-unused-vars */
   if (trueOrFalse) {
-    /* eslint no-unused-vars */
     const task = true;
     const task2 = false;
   }
+  /* eslint-disable no-unused-vars */
 
   return [task, task2];
 }
