1af24f4fcd6c29bed62e87c505f078b206f5543b
vicomarquez <vicomarquezmuniz@gmail.com>
2023-03-27 15:26:35 -0300

    fix task

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,8 +1,8 @@
-function handleResponseFromAPI(promise) {
+export default function handleResponseFromAPI(promise) {
   return new Promise
   .then(() => ({
-    status: 200,
-    body: 'success',
+     status: 200,
+     body: 'success',
   }))
   .catch(() => Error())
   .finally(() => console.log ('Got a response from the API'));
