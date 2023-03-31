2d350994d04644b3604294afbd9fe8144060a5e1
vicomarquez <vicomarquezmuniz@gmail.com>
2023-03-31 09:48:31 -0300

    task 4

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,10 @@
+export default function updateStudentGradeByCity(arr, city, Ngrade) {
+  return arr
+  .filter((obj) => obj.location === city)
+  .map((student) => {
+    const grade = Ngrade
+      .filter((obj) => obj.studentId === student.id)
+      .map((i) => i.grade)[0] || 'N/A';
+    return { ...student, grade };
+  });
+}
