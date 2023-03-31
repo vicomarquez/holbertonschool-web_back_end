b5afb0ee5c04234a0c43f4371d02b0a6d86d04ac
vicomarquez <vicomarquezmuniz@gmail.com>
2023-03-31 10:38:32 -0300

    fix lint

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startString) {
   if (typeof startString !== 'string') return '';
 
   let res = '';
-  
+
   set.forEach((obj) => {
     if (obj && obj.startsWith(startString)) res += `${obj.slice(startString.length)}-`;
   });
