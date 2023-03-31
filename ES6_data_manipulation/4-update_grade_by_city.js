fdc05e9367d0caf4d78a964e0bfc2ac92a2d26f7
vicomarquez <vicomarquezmuniz@gmail.com>
2023-03-31 09:51:04 -0300

    fix lint

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,10 +1,10 @@
 export default function updateStudentGradeByCity(arr, city, Ngrade) {
   return arr
-  .filter((obj) => obj.location === city)
-  .map((student) => {
-    const grade = Ngrade
+    .filter((obj) => obj.location === city)
+    .map((student) => {
+      const grade = Ngrade
       .filter((obj) => obj.studentId === student.id)
-      .map((i) => i.grade)[0] || 'N/A';
-    return { ...student, grade };
-  });
+        .map((i) => i.grade)[0] || 'N/A';
+      return { ...student, grade };
+    });
 }
