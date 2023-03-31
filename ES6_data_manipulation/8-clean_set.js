8aef7fa3c1a257f15889dc77e16176561568b52e
vicomarquez <vicomarquezmuniz@gmail.com>
2023-03-31 10:36:59 -0300

    task 8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,10 @@
 export default function cleanSet(set, startString) {
+  if (typeof startString !== 'string') return '';
+
   let res = '';
+  
   set.forEach((obj) => {
     if (obj && obj.startsWith(startString)) res += `${obj.slice(startString.length)}-`;
-    });
-  return result.slice(0, res.length - 1);
+  });
+  return res.slice(0, res.length - 1);
 }
