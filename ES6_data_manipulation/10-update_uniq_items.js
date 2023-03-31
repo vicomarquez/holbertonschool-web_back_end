4aa8903ed390f5f9dc323331423c1193f445927b
vicomarquez <vicomarquezmuniz@gmail.com>
2023-03-31 11:04:39 -0300

    task 10

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,3 +1,8 @@
-export default function updateUniqueItems() {
-    return
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) throw Error('Cannot process');
+
+  map.forEach((v, k) => {
+    if (v === 1) map.set(k, 100);
+  });
+  return map;
 }
