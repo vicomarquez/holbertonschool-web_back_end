328d36e1929e80a3d153de7dd607df9e4ad25e5d
vicomarquez <vicomarquezmuniz@gmail.com>
2023-03-31 10:54:11 -0300

    task 9

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -1,3 +1,10 @@
 export default function groceriesList() {
-    return
+  const Glist = new Map();
+  Glist.set('Apples', 10);
+  Glist.set('Tomatoes', 10);
+  Glist.set('Pasta', 1);
+  Glist.set('Rice', 1);
+  Glist.set('Banana', 5);
+
+  return Glist;
 }
