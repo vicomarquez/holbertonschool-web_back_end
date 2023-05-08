32d897bac103e2a521855f18004d914087ac09c0
vicomarquez <vicomarquezmuniz@gmail.com>
2023-05-08 17:50:21 -0300

    fix task 3

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -2,8 +2,6 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
     return Promise.all([uploadPhoto(), createUser()])
-    .then((result) => {
-        console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`)
-        .catch(() => console.log('Signup system offline'))
-    }) 
-}
+    .then((result) => console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`))
+    .catch(() => console.log('Signup system offline'));
+    }
