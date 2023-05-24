aebd49e4147744a691c28032a6f08394fd19c3aa
vicomarquez <vicomarquezmuniz@gmail.com>
2023-05-24 16:41:15 -0300

    tasks 6, 7, 8, 9

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,6 +1,31 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
-  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)]);
+
+export default async function handleProfileSignup(firstName='', lastName='', fileName='') {
+  const prom1 = {
+    status: 'pending',
+    value: '',
+  };
+  const prom2 = {
+    status: 'pending',
+    value: '',
+  };
+  try {
+    const signup = await signUpUser(firstName, lastName);
+    prom1.status = 'fulfilled';
+    prom1.value = signup;
+  } catch (err) {
+    prom1.status = 'rejected';
+    prom1.value = err.toString();
+  }
+  try {
+    const upload = await uploadPhoto(fileName);
+    prom2.status = 'fulfilled';
+    prom2.value = upload;
+  } catch (err) {
+    prom2.status = 'rejected';
+    prom2.value = err.toString();
+  }
+  return [prom1, prom2];
 }
