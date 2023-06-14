5fdfc3c7d9919d027826e40d8bca9419b71c05bc
vicomarquez <vicomarquezmuniz@gmail.com>
2023-06-14 17:15:52 -0300

    update tasks

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -5,4 +5,41 @@ export default class HolbertonCourse {
         this._students = students;
     }
 
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set name(name) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw TypeError('Name must be a string')
+    }
+  }
+
+  set length(length) {
+    if (typeof length === 'number') {
+      this._length = length;
+    } else {
+      throw TypeError('Length must be a number')
+    }
+  }
+  
+  set students(students) {
+    students.forEach((student) => {
+      if (typeof student === 'string') {
+        this._students = students;
+      } else {
+        throw TypeError('Students must be an array of strings');
+      }
+    });
+  }
 }
